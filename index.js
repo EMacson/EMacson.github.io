5288e9df73b46558511588b5c306f1f3ef0bea03
ethan m <48036998+EMacson@users.noreply.github.com>
2025-02-27 11:51:30 -0800

    wasd input added

--- index.js
+++ index.js
@@ -50,10 +50,23 @@ const floor = new Entity({
 
 entities.push(floor)
 
+const keys = {
+    d: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    }
+}
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height)
+
+    player.velocity.x = 0
+    if(keys.d.pressed) player.velocity.x = 5
+    else if(keys.a.pressed) player.velocity.x = -5
 
     for(var i = 0, length = entities.length; i < length; i++) {
         entity = entities[i]
@@ -62,3 +75,28 @@ function animate() {
 }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd': 
+            keys.d.pressed = true
+            break
+        case 'a': 
+            keys.a.pressed = true
+            break
+        case 'w': 
+            player.velocity.y = -20
+            break
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd': 
+            keys.d.pressed = false
+            break
+        case 'a': 
+            keys.a.pressed = false
+            break
+    }
+})
